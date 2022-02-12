2741786deaaf0e163fa08f2f9812346ab5c943f7
laubsauger <laubsauger@gmail.com>
2022-02-11 16:52:43 -0800

    handle button click and mouse up; send events for that

--- src/components/Controller/CtrlButton/index.tsx
+++ src/components/Controller/CtrlButton/index.tsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import { observer } from 'mobx-react-lite';
 
 import './styles.scss';
+import {useSocket} from "../../../hooks/useSocket";
 
 type Props = {
   label: string,
   variant: 'black' | 'red' | 'green' | 'blue',
   channelName: string,
+  released: boolean,
 };
 
 const CtrlButton = (props:Props) => {
-  const { label, variant, channelName } = props;
+  const { label, variant, channelName, released } = props;
+  const [ pressed, setPressed ] = useState(false);
+  const socket = useSocket();
+
+  const handleBtnPress = useCallback(() => {
+    setPressed(true);
+    socket.emit('OSC_CTRL_MESSAGE', {
+      message: 'button',
+      btnId: channelName,
+      state: 1,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (pressed && released) {
+      socket.emit('OSC_CTRL_MESSAGE', {
+        message: 'button',
+        btnId: channelName,
+        state: 0,
+      });
+
+      setPressed(false);
+    }
+  }, [ pressed, released ]);
 
   return (
     <button className={`btn btn-${variant}`}
-            onClick={() => console.log('btn:onClick', channelName) }
+            onMouseDown={handleBtnPress}
     >
       { label }
     </button>
--- src/components/Controller/index.tsx
+++ src/components/Controller/index.tsx
@@ -14,15 +14,14 @@ import { useStores } from '../../hooks/useStores';
 
 const Controller = () => {
   const { socketStore } = useStores();
+  const { search } = useLocation();
   const socket = useSocket();
 
-  const { search } = useLocation();
-
   const [ wantsSlot, setWantsSlot ] = useState(0);
+  const [ firedMouseUp, setFiredMouseUp ] = useState(false);
 
   useEffect(() => {
     const query = queryString.parse(search);
-    console.log(query.slot);
     if (query.slot) {
       setWantsSlot(Number(query.slot));
     }
@@ -103,10 +102,6 @@ const Controller = () => {
   }, [ socketStore ]);
 
   useEffect(() => {
-    // @todo: move connection state logic to store; pure logic has no place here
-    // @todo: useSocket there
-    // @todo: BONUS: make sure to not instantiate on homepage but only on Join/Configure
-    // @todo: keeps logic clearer, avoids callback memoization, allows access everywhere-ish
     socketStore.updateConnectionState({
       connecting: true,
     });
@@ -131,19 +126,32 @@ const Controller = () => {
     };
   }, [socket, socketStore, handleConnected, handleDisconnected, handleJoinAccepted, handleJoinRejected, handleUserLeft]);
 
+  const handleMouseUp = useCallback((data:any) => {
+    console.log('UI::mouseUp', data);
+    setFiredMouseUp(true);
+    setFiredMouseUp(false);
+  }, [ socketStore ]);
+
+  useEffect(() => {
+    window.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      window.removeEventListener('mouseup', handleMouseUp)
+    };
+  }, [])
+
   return (
     <div className='Controller'>
       <LogoBackground />
-
       <SessionInfo />
 
       { socketStore.connectionState.joined &&
         <React.Fragment>
           {/* //@todo: create buttons according to config */}
-          <CtrlButton channelName='b1' label='1' variant='black' />
-          <CtrlButton channelName='b2' label='2' variant='red' />
-          <CtrlButton channelName='b3' label='3' variant='green' />
-          <CtrlButton channelName='b4' label='4' variant='blue' />
+          <CtrlButton channelName='b1' label='1' variant='black' released={firedMouseUp}/>
+          <CtrlButton channelName='b2' label='2' variant='red' released={firedMouseUp}/>
+          <CtrlButton channelName='b3' label='3' variant='green' released={firedMouseUp}/>
+          <CtrlButton channelName='b4' label='4' variant='blue' released={firedMouseUp}/>
 
           {/* //@todo: create xy controller canvas if configured */}
           <CtrlXY channelNames={['x','y']}/>
