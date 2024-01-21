14b372b94d45570a349edfd1289394863c7c7d77
laubsauger <laubsauger@gmail.com>
2024-01-21 03:43:30 -0800

    134

--- src/components/Controller/CtrlEden/ImageGallery/ScoresImageGallery.tsx
+++ src/components/Controller/CtrlEden/ImageGallery/ScoresImageGallery.tsx
@@ -45,8 +45,10 @@ const ScoresImageGallery = ({
         renderThumbInner: () => (
           <div className="image-gallery-thumbnail-inner position-relative h-100">
              <img src={image.image} alt="" className="image-gallery-thumbnail-image" />
-            <div className="position-absolute top-0 z-index-above w-100 h-100 d-">
-              <div className="image-gallery-thumbnail-label bg-black position-relative align-self-end" style={{ opacity: '85%', top: '9px' }}><span>Votes:{' '}</span><span className="fw-semibold">{image.votes}</span></div>
+            <div className="position-absolute top-0 z-index-above w-100 h-100">
+              <div className="image-gallery-thumbnail-label bg-black position-relative align-self-end" style={{ opacity: '85%', top: '8px' }}>
+                <span>Votes:{' '}</span><span className="fw-semibold">{image.votes}</span>
+              </div>
               {index === 0 && (
                 <div className="image-gallery-thumbnail-label" style={{ position: 'absolute', top: 'initial', bottom: '-9px' }}>Winner!</div>
               )}
