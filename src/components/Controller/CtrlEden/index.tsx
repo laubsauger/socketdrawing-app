99ce7b897c93cad93264e7881192a7217d627f09
laubsauger <laubsauger@gmail.com>
2024-02-18 13:53:17 -0800

    force z-index on top

--- src/components/Controller/CtrlEden/index.tsx
+++ src/components/Controller/CtrlEden/index.tsx
@@ -145,7 +145,7 @@ const CtrlEden = (props: Props) => {
     <div className="d-flex flex-column overflow-hidden" style={{ height: '100%'}}>
       { !gameStore.userName
         ? (
-          <div className="position-absolute bg-opacity-50 bg-black d-flex flex-column align-items-center justify-content-center z-index-above" style={{ inset: 0 }}>
+          <div className="position-absolute bg-opacity-50 bg-black d-flex flex-column align-items-center justify-content-center" style={{ inset: 0, zIndex: 5000}}>
             <h3>Wanna get prompin'?</h3>
             <UserNameForm />
           </div>
