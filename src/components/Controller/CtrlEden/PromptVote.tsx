fac89b57a2b43a6cf316859a8becc8f47db77fec
laubsauger <laubsauger@gmail.com>
2024-02-28 23:48:12 -0800

    votinbg

--- src/components/Controller/CtrlEden/PromptVote.tsx
+++ src/components/Controller/CtrlEden/PromptVote.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
 import { PromptItem } from '../../../stores/gameStore';
 import { useSocket } from '../../../hooks/useSocket';
+import { Form, Row, Col } from 'react-bootstrap';
 
 const PromptVote = ({ items }: { items: PromptItem[] }) => {
   const socket = useSocket();
@@ -8,28 +9,33 @@ const PromptVote = ({ items }: { items: PromptItem[] }) => {
   const handleVote = useCallback((id: PromptItem['id']) => {
     console.log('handleVote', id)
     socket.emit('OSC_CTRL_MESSAGE', {
-      message: 'audience_action',
+      message: 'action',
       type: 'prompt_vote',
       value: id
     });
   }, [])
 
   return (
-    <div>
-      { items.map(item => {
-        return (
-          <div key={item.id}>
-            <h3>{item.prompt}</h3>
-            <input type="radio" onSelect={(evt) => handleVote(item.id)}/>
-            <label htmlFor={`vote_${item.id}`}>
+    <Form>
+      {items.map((item: PromptItem) => (
+        <Form.Group as={Row} key={item.id}>
+          <Form.Label column sm={10} htmlFor={`vote_${item.id}`} className="d-flex align-items-center">
+            <Form.Check
+              type="radio"
+              id={`vote_${item.id}`}
+              name="promptVote"
+              onChange={() => handleVote(item.id)}
+              inline
+            />
+            <div>
               <div>{item.prompt}</div>
               <div className="text-muted">{item.hint}</div>
-            </label>
-          </div>
-        )
-      })}
-    </div>
+            </div>
+          </Form.Label>
+        </Form.Group>
+      ))}
+    </Form>
   )
 }
 
-export default PromptVote
+export default PromptVote;
